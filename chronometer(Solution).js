9699048f429eaf108f71826feb574707a74968cb
AKBasit <141636098+AKBasit@users.noreply.github.com>
2023-09-09 15:02:57 +0100

    Add files via upload

---
+++ chronometer(Solution).js
@@ -0,0 +1,49 @@
+class Chronometer {
+  constructor() {
+    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+  }
+
+  start(printTimeCallback) {
+    // ... your code goes here
+    this.intervalId = setInterval(() =>{
+      this.currentTime++;
+      if(printTimeCallback)
+      {printTimeCallback()}
+    }, 1000)
+  }
+
+  getMinutes() {
+    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
+  }
+
+  getSeconds() {
+    // ... your code goes here
+    return this.currentTime % 60
+  }
+
+  computeTwoDigitNumber(value) {
+    const stringValue = value +``;
+    if(stringValue.length === 1) {
+      return "0" + stringValue.slice(0)
+    }
+    return stringValue
+  }
+
+  stop() {
+    // ... your code goes here
+    return clearInterval(this.intervalId)
+  }
+
+  reset() {
+    // ... your code goes here
+    this.currentTime = 0
+  }
+
+  split() {
+    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
+}
+}
